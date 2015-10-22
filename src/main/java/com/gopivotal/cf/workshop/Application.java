a340d0e4bac73f3b978e76d692872b75e5d03509
Ajay Koranne <akoranne@gmail.com>
2015-10-22 14:01:53 -0600

    Update Application.java

--- src/main/java/com/gopivotal/cf/workshop/Application.java
+++ src/main/java/com/gopivotal/cf/workshop/Application.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
  */
 public class Application {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(Config.class, args);
     }
